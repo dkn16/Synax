805a152bd48ccc03117949758dc527a3c1ca03b2
dkn16 <dkn16@foxmail.com>
2024-06-24 14:43:51 -0700

    Calc the temperature

--- synax/synax.py
+++ synax/synax.py
@@ -1,12 +1,34 @@
 import jax.numpy as jnp
 import jax
+import numpy as np
+
+import scipy.constants as const
+
+q_converter = 1/(4*np.pi*const.epsilon_0)**0.5
+
+B_converter = (4*np.pi/const.mu_0)**0.5
+
+freq_irrelavent_const = (const.e*q_converter)**3/(const.electron_mass*const.speed_of_light**2)*(1/(np.pi*2))*(np.sqrt(3)/(8*np.pi))*1e19 # moves kpc = 1e19 m here.
+
+elect_combi = 2/3*const.electron_mass*const.speed_of_light/(const.e*q_converter)
+
+kpc = 3.08567758
 
 @jax.jit
-def sync_I_const(freq,spectral_index: float=-2.51):
-    return 1
+def sync_I_const(freq,spectral_index: float=3.):
+    
+    gamma_func_1 = jax.scipy.special.gamma(spectral_index/4.-1/12.)
+    
+    gamma_func_2_process = (2e-4*B_converter)**(spectral_index/2.+0.5)/(spectral_index+1)*jax.scipy.special.gamma(spectral_index/4+19/12.)# the transition from micro-Gauss to tesla is here.
+    
+    omega = 2*jnp.pi*freq*1e9
+    
+    consts = freq_irrelavent_const*(omega*elect_combi)**(0.5-spectral_index/2)*gamma_func_1*gamma_func_2_process
+    
+    return consts*kpc/(2*const.Boltzmann*freq**2*1e18/(const.speed_of_light**2))
 
 @jax.jit
-def sync_emiss_I(freq:float, b_perp: jax.Array,C:jax.Array,spectral_index: float=-2.51):
+def sync_emiss_I(freq:float, b_perp: jax.Array,C:jax.Array,spectral_index: float=2.):
     # calculating the  synchrotron emissivity 
     # Input:
     #   freq (float): frequency to be computed.
@@ -17,13 +39,13 @@ def sync_emiss_I(freq:float, b_perp: jax.Array,C:jax.Array,spectral_index: float
     #   j (jax.Array): parallel emissivity for the synchrotron emission.
     
 
-    return b_perp**(0.5-spectral_index/2)*C*sync_I_const(freq,spectral_index=spectral_index)
+    return b_perp**(0.5+spectral_index/2)*C*sync_I_const(freq,spectral_index=spectral_index)
 
 
 
 # obtaining integration locations
 @jax.jit
-def obtain_positions(theta,phi,obs_coord:tuple[float] = (-8.3,0.,0.06),x_length:float=20,y_length:float=20,z_length:float=5,num_int_points:int=256,epsilon:float=1e-6):
+def obtain_positions(theta,phi,obs_coord:tuple[float] = (-8.3,0.,0.06),x_length:float=20,y_length:float=20,z_length:float=5,num_int_points:int=64,epsilon:float=1e-6):
     
     nx = jnp.sin(theta+epsilon)*jnp.cos(phi+epsilon)
     ny = jnp.sin(theta+epsilon)*jnp.sin(phi+epsilon)
@@ -31,11 +53,13 @@ def obtain_positions(theta,phi,obs_coord:tuple[float] = (-8.3,0.,0.06),x_length:
     
     max_val = jnp.max(jnp.abs(jnp.array([nx/(x_length-obs_coord[0]*jnp.sign(nx)),ny/(y_length-obs_coord[1]*jnp.sign(ny)),nz/(z_length-obs_coord[2]*jnp.sign(nz))])))
     
-    int_points = jnp.linspace(0,1,256)
+    int_points = jnp.linspace(0,1,num_int_points)
     
-    xs = nx/max_val*int_points+obs_coord[0]
-    ys = ny/max_val*int_points+obs_coord[1]
-    zs = nz/max_val*int_points+obs_coord[2]
+    xs = nx/max_val*int_points
+    ys = ny/max_val*int_points
+    zs = nz/max_val*int_points
     
-    return jnp.array([xs,ys,zs])
+    dl = (xs[1]**2+ys[1]**2+zs[1]**2)**0.5
+    
+    return jnp.array([xs+obs_coord[0],ys+obs_coord[1],zs+obs_coord[2]]),dl
 
