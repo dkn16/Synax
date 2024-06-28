eec5d9b972cd557c6fd661f174b5be2c10883c2f
dkn16 <dkn16@foxmail.com>
2024-06-28 15:40:31 -0700

    optimization added

--- synax/synax.py
+++ synax/synax.py
@@ -1,18 +1,21 @@
 import jax.numpy as jnp
 import jax
 import numpy as np
-
+from functools import partial
 import scipy.constants as const
 
 q_converter = 1/(4*np.pi*const.epsilon_0)**0.5
 
 B_converter = (4*np.pi/const.mu_0)**0.5
 
-freq_irrelavent_const = (const.e*q_converter)**3/(const.electron_mass*const.speed_of_light**2)*(1/(np.pi*2))*(np.sqrt(3)/(8*np.pi))*1e19 # moves kpc = 1e19 m here.
+freq_irrelavent_const = (const.e*q_converter)**3/(const.electron_mass*const.speed_of_light**2)*(np.sqrt(3)/(8*np.pi))*1e19 # moves kpc = 1e16 km here.
 
 elect_combi = 2/3*const.electron_mass*const.speed_of_light/(const.e*q_converter)
 
 kpc = 3.08567758
+
+temp_covert = (const.hbar*1e9)/(const.Boltzmann*2.725)
+#return _t*(np.exp(p)-1.)**2/(p**2*np.exp(p))
 
 @jax.jit
 def sync_I_const(freq,spectral_index: float=3.):
@@ -23,12 +26,16 @@ def sync_I_const(freq,spectral_index: float=3.):
     
     omega = 2*jnp.pi*freq*1e9
     
-    consts = freq_irrelavent_const*(omega*elect_combi)**(0.5-spectral_index/2)*gamma_func_1*gamma_func_2_process
+    freq_irrelavent = freq_irrelavent_const/(2*const.Boltzmann*freq**2*1e18/(const.speed_of_light**2))
     
-    return consts*kpc/(2*const.Boltzmann*freq**2*1e18/(const.speed_of_light**2))
+    consts = freq_irrelavent*(omega*elect_combi)**(0.5-spectral_index/2)*gamma_func_1*gamma_func_2_process
+    
+    p = freq*temp_covert
+    
+    return consts*kpc*(jnp.exp(p)-1.)**2/(p**2*jnp.exp(p))/2
 
 @jax.jit
-def sync_emiss_I(freq:float, b_perp: jax.Array,C:jax.Array,spectral_index: float=2.):
+def sync_emiss_I(freq:float, b_perp: jax.Array,C:jax.Array,spectral_index: float=3.):
     # calculating the  synchrotron emissivity 
     # Input:
     #   freq (float): frequency to be computed.
@@ -39,27 +46,29 @@ def sync_emiss_I(freq:float, b_perp: jax.Array,C:jax.Array,spectral_index: float
     #   j (jax.Array): parallel emissivity for the synchrotron emission.
     
 
-    return b_perp**(0.5+spectral_index/2)*C*sync_I_const(freq,spectral_index=spectral_index)
+    return b_perp**(0.5+spectral_index*0.5)*C*sync_I_const(freq,spectral_index=spectral_index)
 
 
 
 # obtaining integration locations
-@jax.jit
-def obtain_positions(theta,phi,obs_coord:tuple[float] = (-8.3,0.,0.06),x_length:float=20,y_length:float=20,z_length:float=5,num_int_points:int=64,epsilon:float=1e-6):
+@partial(jax.jit, static_argnums=(2,3,4,5,6,7))
+def obtain_positions(theta,phi,obs_coord:tuple[float] = (-8.3,0.,0.006),x_length:float=20,y_length:float=20,z_length:float=5,num_int_points:int=256,epsilon:float=1e-7):
     
-    nx = jnp.sin(theta+epsilon)*jnp.cos(phi+epsilon)
-    ny = jnp.sin(theta+epsilon)*jnp.sin(phi+epsilon)
-    nz = jnp.cos(theta+epsilon)
+    nx = jnp.sin(theta)*jnp.cos(phi)
+    ny = jnp.sin(theta)*jnp.sin(phi)
+    nz = jnp.cos(theta)
     
     max_val = jnp.max(jnp.abs(jnp.array([nx/(x_length-obs_coord[0]*jnp.sign(nx)),ny/(y_length-obs_coord[1]*jnp.sign(ny)),nz/(z_length-obs_coord[2]*jnp.sign(nz))])))
     
-    int_points = jnp.linspace(0,1,num_int_points)
+    int_points,step = jnp.linspace(0,1,num_int_points,endpoint=False,retstep=True)
+    
+    int_points = int_points + step*0.5
     
     xs = nx/max_val*int_points
     ys = ny/max_val*int_points
     zs = nz/max_val*int_points
     
-    dl = (xs[1]**2+ys[1]**2+zs[1]**2)**0.5
+    dl = (xs[0]**2+ys[0]**2+zs[0]**2)**0.5*2
     
     return jnp.array([xs+obs_coord[0],ys+obs_coord[1],zs+obs_coord[2]]),dl
 
